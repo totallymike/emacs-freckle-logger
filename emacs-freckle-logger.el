00da0d45798bb79be1708b4fd0990ca50c44912f
Michael Westbom <totallymike@gmail.com>
2015-01-27 10:04:05 -0500

    It now works!

--- emacs-freckle-logger.el
+++ emacs-freckle-logger.el
@@ -6,38 +6,49 @@
 
 ;;; Code:
 
-(use-package request
-  :ensure request)
+(eval-when-compile (require 'cl))
+(require 'cl-lib)
+(require 'json)
 (require 'request)
 
-(defun get-projects ()
-  (let* ((url "https://apitest.letsfreckle.com/api/projects.json")
-         (token "lx3gi6pxdjtjn57afp8c2bv1me7g89j"))
+(defgroup freckle nil
+  "Log your Freckle hours in Emacs"
+  :group 'comm
+  :prefix "freckle-logger-")
+
+(defcustom freckle-personal-access-token nil
+  "Your access token"
+  :link '(url-link "http://help.letsfreckle.com/import-export-api/api")
+  :group 'freckle)
+
+(defvar freckle-projects-cache ()
+  "The cache of projects, in the form of alists
+
+This way we don't have to request them each time, which is slow.")
+
+(defun freckle-logger--cache-freckle-project (project)
+  "Add project id and name to cache"
+  (let ((id (cdr (assoc 'id project)))
+        (name (cdr (assoc 'name project))))
+    (setq freckle-projects-cache (plist-put freckle-projects-cache name id))))
+
+(defun freckle-logger--get-projects ()
+  (message "%S" freckle-personal-access-token)
+  (let* ((url "https://api.letsfreckle.com/v2/projects"))
     (request
      url
-     :headers '(("X-FreckleToken" . "lx3gi6pxdjtjn57afp8c2bv1me7g89j"))
-     :parser 'json-read
+     :headers `(("X-FreckleToken" . ,freckle-personal-access-token))
+     :parser (lambda ()
+               (let ((json-array-type 'list))
+                 (json-read)))
      :success (function*
                (lambda (&key data &allow-other-keys)
-                (message "%S" data)))
+                 (dolist (project data)
+                   (cache-freckle-project project))))
      :error (function*
-             (lambda (&key data &allow-other-keys)
-               (message data)
-               (message err)
-               (message status))))))
+             (lambda (&key data error-thrown &allow-other-keys)
+               (message "%S" error-thrown))))))
 
-(get-projects)
-
-(request "https://apitest.letsfreckle.com/api/projects.json"
-         :type "GET"
-         :headers '(("X-FreckleToken" . "lx3gi6pxdjtjn57afp8c2bv1me7g89j"))
-         :parser 'json-read
-         :success (function*
-                   (lambda (&key data &allow-other-keys)
-                     (message "Here's a thing: %S" (assoc-default 'json data))))
-         :error (function*
-                 (lambda (data err status)
-                   (message (request-response-data))))
 
 (provide 'emacs-freckle-logger)
 
